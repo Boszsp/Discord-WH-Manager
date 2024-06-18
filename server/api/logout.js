c7ec6d263b33bfc51bc3f79145662f7aee50133d
Boszsp <95701554+Boszsp@users.noreply.github.com>
2024-06-18 12:19:31 +0700

    chore:remove backend function

--- server/api/logout.js
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const runtimeConfig = useRuntimeConfig();
-  const session = await useSession(event, {
-    password: runtimeConfig.backendPassword,
-  });
-  await session.clear();
-  return {status: 200, mss: "success"};
-});
