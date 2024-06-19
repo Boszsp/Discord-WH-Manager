4e08d3b710f2a45de1f23fbb32791e455f02fe4b
Boszsp <95701554+Boszsp@users.noreply.github.com>
2024-06-19 18:23:08 +0700

    fix(component statictable):responsive

--- components/StaticTable.vue
+++ components/StaticTable.vue
@@ -6,8 +6,8 @@ const search = ref("");
 
 <template>
   <v-sheet class="rounded-lg bg-component-background" :elevation="2" rounded>
-    <div class="flex justify-between items-center px-4 py-2">
-      <h4 class="text-h6 font-bold">{{ title ?? "Static Table" }}</h4>
+    <div class="flex justify-between items-center px-4 py-2 overflow-x-auto">
+      <h4 class="text-h6 font-bold whitespace-nowrap mr-4">{{ title ?? "Static Table" }}</h4>
       <span class="flex gap-4 items-center">
         <span class="w-52">
           <v-text-field v-model="search" label="Search" color="primary" class="bg-background-tertiary" prepend-inner-icon="mdi-magnify" density="compact" variant="outlined" size="small" hide-details></v-text-field>
